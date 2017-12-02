18a085ba65a9265ed152e837d0bc1cc142a422df
Joe <jsbenning4@gmail.com>
2017-12-02 15:11:34 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import UserInput from './components/UserInput'
+import UserInput from './components/UserInput';
+import Users from './components/Users';
 
-export class App extends Component {
+
+class App extends Component {
   render() {
     return (
       <div className="App">
         <UserInput store={this.props.store}/>
-        {/* is there something we could connect to here? */}
+        <Users store={this.props.store} />
       </div>
     );
   }
 }
+
+//const myState = this.props.store.getState();
 
 export default App;
--- src/components/Users.js
+++ src/components/Users.js
@@ -3,17 +3,27 @@ import { connect } from 'react-redux';
 
 export class Users extends Component {
 
-  render() {
 
+
+  render() {
+    const allUsers = this.props.store.users;
     return (
       <div>
         <ul>
-          {/* stuff should happen around here */}
+        {this.props.store.users}
+          /* stuff should happen around here */
+        
+
         </ul>
       </div>
     )
   }
 }
 
-export const ConnectedUsers = Users // aren't we supposed to be connecting something around here?
+const mapStateToProps = (state) => {
+  return { users: state.users };
+};
 
+export default connect(mapStateToProps)(Users)
+
+//export const ConnectedUsers = Users // aren't we supposed to be connecting something around here?
